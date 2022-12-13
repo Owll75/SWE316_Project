ba3459eb6735047ec2fc2eab7bde32ca6651ef3a
OWLL75 <7soony2818@gmail.com>
2022-12-13 21:31:02 +0300

    Initial commit

---
+++ src/main/java/com/example/hw316/Stage.java
@@ -0,0 +1,72 @@
+package com.example.hw316;
+
+import java.util.Date;
+
+public class Stage {
+
+    private String objValue;
+    private int docNumber;
+    private int oldValue;
+    private int newValue;
+    private Date date;
+
+    public Stage() {
+
+    }
+
+    public void setObjValue(String objValue) {
+        this.objValue = objValue;
+    }
+
+    public void setDocNumber(int docNumber) {
+        this.docNumber = docNumber;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setOldValue(int oldValue) {
+        this.oldValue = oldValue;
+    }
+
+    public void setNewValue(int newValue) {
+        this.newValue = newValue;
+    }
+
+    public String getObjValue() {
+        return objValue;
+    }
+
+    public String getColor() {
+
+        return "";
+    }
+
+    public int getDocNumber() {
+        return docNumber;
+    }
+
+    public int getOldValue() {
+        return oldValue;
+    }
+
+    public int getNewValue() {
+        return newValue;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    @Override
+    public String toString() {
+        return "Stage{"
+                + "objValue='" + objValue + '\''
+                + ", docNumber=" + docNumber
+                + ", oldValue=" + oldValue
+                + ", newValue=" + newValue
+                + ", Date=" + date
+                + '}';
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.project316 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.apache.poi.poi;
+
+
+    opens com.example.hw316 to javafx.fxml;
+    exports com.example.hw316;
+}
